4fa403cf399a40d7813b384b90e91dd73e65d73f
Annabia <annabbarbosa2712@gmail.com>
2025-02-16 08:35:33 -0300

    fazendo not-found

---
+++ src/app/not-found/page.tsx
@@ -0,0 +1,12 @@
+import { useEffect } from "react";
+
+const NotFoundPage = () => {
+    useEffect(() => {
+        const elemento = document.querySelector("...");
+        // Seu código aqui
+    }, []);
+
+    return <div>Página não encontrada</div>;
+}
+
+export default NotFoundPage;
